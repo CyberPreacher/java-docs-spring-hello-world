ae01f0f855729aabbf040413d3dbb0085d533ad3
CyberPreacher <110272841+CyberPreacher@users.noreply.github.com>
2022-08-10 05:48:56 +0200

    joo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,11 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(neu.class, args);
 	}
 
-	@RequestMapping("/")
-	String sayHello() {
-		return "Hello World!";
-	}
 }
