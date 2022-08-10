e8c2c9eddffc05440e572c53e27a5c1fa74616a4
CyberPreacher <110272841+CyberPreacher@users.noreply.github.com>
2022-08-10 05:54:16 +0200

    juiop

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-
+.
 	public static void main(String[] args) {
 		SpringApplication.run(neu.class, args);
 	}
